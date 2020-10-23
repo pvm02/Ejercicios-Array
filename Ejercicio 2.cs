05a71f99618b74758d7ada165b3c0ebde09bdc0a
Pamela Villar <71789070+pvm02@users.noreply.github.com>
2020-10-22 22:09:47 -0400

    Delete Ejercicio 2.cs

--- Ejercicio 2.cs
+++
@@ -1,3 +0,0 @@
-﻿1>------ Operación Compilar iniciada: proyecto: Array3, configuración: Debug Any CPU ------
-1>  Array3 -> C:\Users\PEDRO\source\repos\Array3\Array3\bin\Debug\Array3.exe
-========== Compilar: 1 correctos, 0 incorrectos, 0 actualizados, 0 omitidos ==========
