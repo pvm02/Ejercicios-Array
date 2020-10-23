3327fd37caf90a051b2580ed1af52e6fbdad9caa
Pamela Villar <71789070+pvm02@users.noreply.github.com>
2020-10-22 23:20:47 -0400

    Add files via upload

---
+++ ejercicio2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrayss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] Num1 = new float[5] { 23, 10, 100, 21, 90 };
+
+            Num1[4] = 23;
+
+            Num1[3] = 10;
+
+            Num1[2] = 100;
+
+            Num1[1] = 21;
+
+            Num1[0] = 90;
+
+            for (int i = 0; i < Num1.Length; i++)
+            {
+                Console.WriteLine(Num1[i]);
+            }
+        }
+    }
+}
